f6605d4e4431034913c151f2bde171f1856124a8
HEND_HAMED\hendhamed <hendhamedmorsy@gmail.com>
2024-04-20 02:55:49 +0200

    updating app helpers model

--- lib/core/constants/db_constants.dart
+++ lib/core/constants/db_constants.dart
@@ -2,16 +2,17 @@
 
 class DbConstants {
   DbConstants._();
-  static const String USER_DATA_KEY = "user_data_key";
-  static const String USER_BOX_KEY = "user_box";
-  static const String TOKEN_BOX_KEY = "token_box";
-  static const String PUSH_TOKEN_BOX_KEY = "push_token_box";
-  static const String LOCAL_NOTIFICATION_LIST_BOX_KEY =
-      "local_notification_list_box_key";
+  static const String PATIENT_DATA_KEY = "patient_data_key";
+  static const String PATIENT_BOX_KEY = "patient_box";
+  static const String PATIENT_TOKEN_BOX_KEY = "patient_token_box";
+
+
+  static const String DOCTOR_DATA_KEY = "doctor_data_key";
+  static const String DOCTOR_BOX_KEY = "doctor_box";
+  static const String DOCTOR_TOKEN_BOX_KEY = "doctor_token_box";
+
+
   static const String ENCRYPTED_BOX = "encrypted_box";
-  static const String ACCESS_TOKEN_ENCRYPTION_KEY =
-      "access_token_encryption_key";
+  static const String ACCESS_TOKEN_ENCRYPTION_KEY = "access_token_encryption_key";
   static const String ACCESS_TOKEN = "access_token";
-  static const String HAS_ACCEPT_POLICY = "has_accept_policy";
-  static const String THEME_BOX_KEY = "theme_box";
 }
